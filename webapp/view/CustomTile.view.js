999c21b0e33b32eac61ac5a89f69786e4b61d815
setusaxena <setu.saxena@sap.com>
2023-08-15 07:06:05 +0200

    changes for CSS

--- webapp/view/CustomTile.view.js
+++ webapp/view/CustomTile.view.js
@@ -1,123 +1,141 @@
 sap.ui.define([
-    "sap/m/GenericTile",
-    "sap/m/TileContent",
-    "sap/m/library",
-    "sap/m/NumericContent"
-], function (
-    GenericTile,
-    TileContent,
-    mobileLibrary,
-    NumericContent
+	"sap/m/GenericTile",
+	"sap/m/TileContent",
+	"sap/m/library",
+	"sap/m/NumericContent"
+], function(
+	GenericTile,
+	TileContent,
+	mobileLibrary,
+	NumericContent
 ) {
-    "use strict";
+	"use strict";
 
-    // shortcut for sap.m.ValueColor
-    var ValueColor = mobileLibrary.ValueColor;
+	// shortcut for sap.m.ValueColor
+	var ValueColor = mobileLibrary.ValueColor;
 
-    sap.ui.jsview("view.CustomTile", {
-        getControllerName: function () {
-            return "view.CustomTile";
-        },
+	sap.ui.jsview("view.CustomTile", {
+		getControllerName: function() {
+			return "view.CustomTile";
+		},
 
-        createContent: function () {
-            this.setHeight("100%");
-            this.setWidth("100%");
+		createContent: function() {
+			this.setHeight("100%");
+			this.setWidth("100%");
+			var tileControl = this.getTileControl();
+			return tileControl;
+		},
 
-            return this.getTileControl();
-        },
+		_addCSSFile: function(url) {
+			return new Promise((resolve, reject) => {
+				let link = document.createElement('link');
+				link.type = 'text/css';
+				link.rel = 'stylesheet';
+				link.onload = () => {
+					resolve();
+					console.log('style has loaded');
+				};
+				link.href = url;
 
-        getTileControl: function () {
-            var oController = this.getController();
+				let headScript = document.querySelector('script');
+				headScript.parentNode.insertBefore(link, headScript);
+			});
+		},
 
-            return new GenericTile({
-                mode: "{/mode}",
-                header: "{/data/display_title_text}",
-                subheader: "{/data/display_subtitle_text}",
-                size: "Auto",
-                sizeBehavior: "{/sizeBehavior}",
-                wrappingType: "{/wrappingType}",
-                tileContent: [new TileContent({
-                    size: "Auto",
-                    footer: "{/data/display_info_text}",
-                    footerColor: {
-                        path: "/data/display_info_state",
-                        formatter: function (sFooterColor) {
-                            if (sFooterColor === "Positive") {
-                                sFooterColor = ValueColor.Good;
-                            }
-                            if (sFooterColor === "Negative") {
-                                sFooterColor = ValueColor.Error;
-                            }
+		getTileControl: function() {
+			var oController = this.getController();
 
-                            if (!ValueColor[sFooterColor]) {
-                                sFooterColor = ValueColor.Neutral;
-                            }
+			return new GenericTile({
+				mode: "{/mode}",
+				header: "{/data/display_title_text}",
+				subheader: "{/data/display_subtitle_text}",
+				size: "Auto",
+				sizeBehavior: "{/sizeBehavior}",
+				wrappingType: "{/wrappingType}",
+				tileContent: [new TileContent({
+					size: "Auto",
+					footer: "{/data/display_info_text}",
+					footerColor: {
+						path: "/data/display_info_state",
+						formatter: function(sFooterColor) {
+							if (sFooterColor === "Positive") {
+								sFooterColor = ValueColor.Good;
+							}
+							if (sFooterColor === "Negative") {
+								sFooterColor = ValueColor.Error;
+							}
 
-                            return sFooterColor;
-                        }
-                    },
-                    unit: "{/data/display_number_unit}",
-                    //We'll utilize NumericContent for the "Dynamic" content.
-                    content: [new NumericContent({
-                        scale: "{/data/display_number_factor}",
-                        value: "{/data/display_number_value}",
-                        truncateValueTo: 5, //Otherwise, The default value is 4.
-                        indicator: "{/data/display_state_arrow}",
-                        valueColor: {
-                            path: "/data/display_number_state",
-                            formatter: function (sValueColor) {
-                                if (!ValueColor[sValueColor]) {
-                                    sValueColor = ValueColor.Neutral;
-                                }
-                                return sValueColor;
-                            }
-                        },
-                        icon: "{/data/display_icon_url}",
-                        width: "100%"
-                    })]
-                })],
-                press: [oController.onPress, oController]
-            });
-        },
+							if (!ValueColor[sFooterColor]) {
+								sFooterColor = ValueColor.Neutral;
+							}
 
-        /*
-        We should change the color of the text in the footer ("info") to be as received in the tile data in the property (infostate).
-        We used to have this functionality when we used the BaseTile. (we added a class which change the text color).
-        Today The GenericTile doesn't support this feature, and it is impossible to change the text color.
-        Since this feature is documented, we should support it - See BCP:1780008386.
-        */
-        onAfterRendering: function () {
-            var oModel = this.getModel(),
-                sDisplayInfoState = oModel.getProperty("/data/display_info_state"),
-                elDomRef = this.getDomRef(),
-                elFooterInfo = elDomRef ? elDomRef.getElementsByClassName("sapMTileCntFtrTxt")[0] : null;
+							return sFooterColor;
+						}
+					},
+					unit: "{/data/display_number_unit}",
+					//We'll utilize NumericContent for the "Dynamic" content.
+					content: [new NumericContent({
+						scale: "{/data/display_number_factor}",
+						value: "{/data/display_number_value}",
+						truncateValueTo: 5, //Otherwise, The default value is 4.
+						indicator: "{/data/display_state_arrow}",
+						valueColor: {
+							path: "/data/display_number_state",
+							formatter: function(sValueColor) {
+								if (!ValueColor[sValueColor]) {
+									sValueColor = ValueColor.Neutral;
+								}
+								return sValueColor;
+							}
+						},
+						icon: "{/data/display_icon_url}",
+						width: "100%"
+					})]
+				})],
+				press: [oController.onPress, oController]
+			});
+		},
 
-            if (elFooterInfo) {
-                switch (sDisplayInfoState) {
-                    case "Negative":
-                        //add class for Negative.
-                        elFooterInfo.classList.add("sapUshellTileFooterInfoNegative");
-                        break;
-                    case "Neutral":
-                        //add class for Neutral.
-                        elFooterInfo.classList.add("sapUshellTileFooterInfoNeutral");
-                        break;
-                    case "Positive":
-                        //add class for Positive.
-                        elFooterInfo.classList.add("sapUshellTileFooterInfoPositive");
-                        break;
-                    case "Critical":
-                        //add class for Critical.
-                        elFooterInfo.classList.add("sapUshellTileFooterInfoCritical");
-                        break;
-                    default:
-                        return;
-                }
-            }
-        },
+		/*
+		We should change the color of the text in the footer ("info") to be as received in the tile data in the property (infostate).
+		We used to have this functionality when we used the BaseTile. (we added a class which change the text color).
+		Today The GenericTile doesn't support this feature, and it is impossible to change the text color.
+		Since this feature is documented, we should support it - See BCP:1780008386.
+		*/
+		onAfterRendering: function() {
+			var oModel = this.getModel(),
+				sDisplayInfoState = oModel.getProperty("/data/display_info_state"),
+				elDomRef = this.getDomRef(),
+				elFooterInfo = elDomRef ? elDomRef.getElementsByClassName("sapMTileCntFtrTxt")[0] : null;
 
-        getMode: function () {
-            return this.getModel().getProperty("/mode");
-        }
-    });
+			if (elFooterInfo) {
+				switch (sDisplayInfoState) {
+					case "Negative":
+						//add class for Negative.
+						elFooterInfo.classList.add("sapUshellTileFooterInfoNegative");
+						break;
+					case "Neutral":
+						//add class for Neutral.
+						elFooterInfo.classList.add("sapUshellTileFooterInfoNeutral");
+						break;
+					case "Positive":
+						//add class for Positive.
+						elFooterInfo.classList.add("sapUshellTileFooterInfoPositive");
+						break;
+					case "Critical":
+						//add class for Critical.
+						elFooterInfo.classList.add("sapUshellTileFooterInfoCritical");
+						break;
+					default:
+						return;
+				}
+			}
+			debugger;
+			this._addCSSFile();
+		},
+
+		getMode: function() {
+			return this.getModel().getProperty("/mode");
+		}
+	});
 });
